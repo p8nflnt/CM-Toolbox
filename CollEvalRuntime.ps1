b8bb0b5c2d22f4ef87724539e5facde21eb88c62
Payton Flint <47405653+pflint@users.noreply.github.com>
2023-05-15 15:50:08 -0500

    Create CollEvalRuntime.ps1

---
+++ CollEvalRuntime.ps1
@@ -0,0 +1,33 @@
+# Identify location of script
+$ScriptPath = Split-Path ($MyInvocation.MyCommand.Path) -Parent
+
+# Set collection ID list location
+$AllCollIDs = Get-Content "$ScriptPath\IDList.txt"
+
+# Install/check for ConfigurationManager module
+try {
+    Import-Module ConfigurationManager -ErrorAction 'Stop'
+    Set-Location CAS:
+}
+catch [System.IO.FileNotFoundException] {
+    throw 'The ConfigurationManager module cannot be found.'
+}
+
+# Run through list of IDs
+$Output = foreach ($ID in $AllCollIDs) {
+
+    # Get duration from CollEval & convert to seconds
+    $Length = [Math]::Round((Get-CMCollectionFullEvaluationStatus -ID $ID | Select-Object -ExpandProperty Length)/1000)
+
+    # Create table
+    [PSCustomObject]@{
+        Name =   $ID
+        Length = $Length
+    }
+}
+
+# Revert to local
+Set-Location $env:SystemDrive
+
+# Write output to .CSV at parent directory
+$Output | Export-Csv -Path "$ScriptPath\CollEvalRunTime_Output.csv" -NoTypeInformation
